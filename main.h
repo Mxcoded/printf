caf42542debb9edc0d6718625ee8525298c5bc3a
Chidi <chidiamaraekejiuba@gmail.com>
2022-11-22 07:24:19 -0800

    Initial Commit Header File

---
+++ main.h
@@ -0,0 +1,24 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <unistd.h>
+
+/**
+ * Prototype Functions
+ */
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+int fn_char(va_list c);
+int fn_string(va_list s);
+int fn_int(va_list i);
+int fn_dec(va_list d);
+
+/**
+ * Added the remaining parts based on what i told you, then notify me for corrections
+ */
+
+#endif
