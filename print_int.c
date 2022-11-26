856c9abafb176a033bd465d2f70616a06d32de10
Chidi <chidiamaraekejiuba@gmail.com>
2022-11-25 20:51:06 -0800

    printf project

--- print_int.c
+++ print_int.c
@@ -1,48 +1,19 @@
 #include "main.h"
-/**
-* fn_int - function that prints an integer
-* @i: integer to print
-* Descriptions: prints digit with _putchar
-* Return: size the output text
-*/
-int fn_int(va_list i)
-{
-	int len, powten, j, digit, n, count = 0, num;
 
-	n = va_arg(i, int);
-	if (n != 0)
-	{
-		if (n < 0)
-		{
-			_putchar('-');
-			count++;
-		}
-		num = n;
-		len = 0;
-		while (num != 0)
-		{
-			num /= 10;
-			len++;
-		}
-		powten = 1;
-		for (j = 1; j <= len - 1; j++)
-			powten *= 10;
-		for (j = 1; j <= len; j++)
-		{
-			digit = n / powten;
-			if (n < 0)
-				_putchar((digit * -1) + 48);
-			else
-				_putchar(digit + '0');
-			count++;
-			n -= digit * powten;
-			powten /= 10;
-		}
-	}
-	else
-	{
-		_putchar('0');
-		return (1);
-	}
-	return (count);
+/**
+ * print_integer - Print a number in base 10
+ * @list: Number to print in base 10
+ *
+ * Return: Length of th numbers in decimal
+ **/
+int print_integer(va_list list)
+{
+	char *p_buff;
+	int size;
+
+	p_buff = itoa(va_arg(list, int), 10);
+
+	size = print((p_buff != NULL) ? p_buff : "NULL");
+
+	return (size);
 }
